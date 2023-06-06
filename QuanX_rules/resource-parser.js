a5b3404656370d49a5c57554084806c084c9b5fe
Pukia <70846484+Pukia-vj@users.noreply.github.com>
2023-06-06 16:13:33 +0800

    Update resource-parser.js

--- QuanX_rules/resource-parser.js
+++ QuanX_rules/resource-parser.js
@@ -1,1 +1,7 @@
 
+// CDN加速
+function CDN(cnt) {
+  console.log("CDN start")
+  cnt = cnt.join("\n").replace(/https:\/\/raw.githubusercontent.com\/(.*?)\/(.*?)\/(.*)/gmi,"https://fastly.jsdelivr.net/gh/$1/$2@$3")
+  return cnt
+}
